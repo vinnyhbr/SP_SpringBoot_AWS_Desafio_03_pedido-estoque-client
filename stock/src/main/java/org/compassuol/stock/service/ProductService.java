ffaa91b807fa4728a34d85a8853ef614b5b5e607
vinnyhbr <113705995+vinnyhbr@users.noreply.github.com>
2024-11-27 11:05:04 -0300

    feat: criando productservice

---
+++ stock/src/main/java/org/compassuol/stock/service/ProductService.java
@@ -0,0 +1,16 @@
+package org.compassuol.stock.service;
+
+import lombok.RequiredArgsConstructor;
+import org.compassuol.stock.domain.Product;
+import org.compassuol.stock.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository repository;
+
+    public Product save(Product product){
+        return repository.save(product);
+    }
+}
