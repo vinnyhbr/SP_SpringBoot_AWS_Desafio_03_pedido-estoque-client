3fa7035c000db46a309275805005d8a7933eb3ae
vinnyhbr <113705995+vinnyhbr@users.noreply.github.com>
2024-12-03 11:56:07 -0300

    delete order

--- order/src/main/java/org/compassuol/order/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package org.compassuol.order.repository;
-
-import org.compassuol.order.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
